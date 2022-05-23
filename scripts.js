4d2c013297f356971c7dcf12e0e2554c17330b1c
imsourceboom <ecmascript.developer@gmail.com>
2022-05-23 10:46:41 +0900

    add logo and favicons

--- scripts.js
+++ scripts.js
@@ -22,23 +22,27 @@ var throttling = function throttling(f) {
   };
 };
 
-var button = document.createElement('a');
-
-button.setAttribute('href', '#body');
-button.style.cssText =
-  '\n    position: fixed;\n    bottom: 6vh;\n    right: 5vw;\n    z-index: 999999;\n    display: none;\n    width: 50px;\n    height: 50px;\n    border-radius: 50%;\n    box-shadow: 2px 2px 8px rgba(169, 239, 241, 0.432);\n    backdrop-filter: blur(4px);\n    background-image: url(https://icongr.am/clarity/arrow.svg?size=30&color=6b6b6b);\n    background-position: center;\n    background-size: 25px;\n    background-repeat: no-repeat;\n';
-
-document.body.appendChild(button);
-
+var button = document.getElementById('gototop');
 window.addEventListener(
   'scroll',
   throttling(function () {
     var pageY = window.pageYOffset;
 
     if (pageY >= 55) {
-      button.style.display = 'block';
+      button.classList.add('show');
     } else {
-      button.style.display = 'none';
+      button.classList.remove('show');
     }
   })
 );
+
+var moveGoToTop = function moveGoToTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+};
+
+button.addEventListener('click', moveGoToTop);
